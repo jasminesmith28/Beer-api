44b596893ed0f4b88abe39931b1dd0f53f541206
Jasmine Smith <jasmine.smith28@outlook.com>
2018-11-06 19:32:44 +0000

    appended html

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -17,44 +17,77 @@ var getData = function getData(image, heading, description, abv, ibu, ph, taglin
 
 ;
 
+var ingredients = function ingredients(malt, hops, yeast) {
+    _classCallCheck(this, ingredients);
+
+    this.malt = malt;
+    this.hops = hops;
+    this.yeast = yeast;
+};
+
+;
+
 $('body').on('click', '.page-number', function () {
     $('.page-number').removeClass("active");
     $(this).addClass("active");
+    $('main').empty();
+    getBeer();
 });
 
 $('body').on('click', '.next', function () {
     if ($('.active').next().hasClass('page-number')) {
         $('.active').removeClass('active').next().addClass('active');
     }
+    $('main').empty();
+    getBeer();
 });
 
 $('body').on('click', '.previous', function () {
     if ($('.active').prev().hasClass('page-number')) {
         $('.active').removeClass('active').prev().addClass('active');
     }
+    $('main').empty();
+    getBeer();
 });
 
-var url = "https://api.punkapi.com/v2/beers?page=" + $('.active').text() + "&per_page=6";
-$.ajax({
-    url: url,
-    method: 'GET'
-}).done(function (data) {
-    var beers = [];
-    data.forEach(function (element) {
-        beers.push(new getData(element.image_url, element.name, element.description, element.abv, element.ibu, element.ph));
+function getBeer() {
+    var url = "https://api.punkapi.com/v2/beers";
+    url += '?' + $.param({
+        'page': $('.active').text(),
+        'per_page': 6
     });
-    console.log(beers);
-});
+
+    $.ajax({
+        url: url,
+        method: 'GET'
+    }).done(function (data) {
+        var beers = [];
+        data.forEach(function (element) {
+            beers.push(new getData(element.image_url, element.name, element.description, element.abv, element.ibu, element.ph));
+            $('main').append('<div class="beer-item"><img src="' + element.image_url + '" ><h1>' + element.name + '</h1><p>' + element.description + '</p><section><ul><li>ABV</li><li><span>' + element.abv + '</span></li></ul><ul><li>IBU</li><li><span>' + element.ibu + '</span></li></ul><ul class="ph-color"><li>PH</li><li><span>' + element.ph + '</span></li></ul></section></div>');
+        });
+        console.log(beers);
+    });
+}
 
 $.ajax({
     url: "https://api.punkapi.com/v2/beers/random",
     method: 'GET'
 }).done(function (data) {
     var randomBeer = [];
+    var flavour = [];
     data.forEach(function (element) {
         randomBeer.push(new getData(element.image_url, element.name, element.description, element.abv, element.ibu, element.ph, element.tagline));
+        $('.randomBeer').append('<div class="one-beer"><div><img src="' + element.image_url + '" ></div><div class="description"><h1>' + element.name + '</h1><h2>' + element.tagline + '</h2><p>' + element.description + '</p><section><ul><li>ABV</li><li><span>' + element.abv + '</span></li></ul><ul><li>IBU</li><li><span>' + element.ibu + '</span></li></ul><ul class="ph-color"><li>PH</li><li><span>' + element.ph + '</span></li></ul></section><button>INGREDIENTS</button></div></div><button>GIVE ME ANOTHER BEER</button>');
+        flavour.push(new ingredients(element.ingredients.malt, element.ingredients.hops, element.ingredients.yeast));
     });
+
     console.log(randomBeer);
+    console.log(flavour);
+});
+
+$('.beerButton').on('click', function () {
+    $('main').css('display', 'none');
 });
 
 },{}]},{},[1]);
--- src/js/index.js
+++ src/js/index.js
@@ -11,23 +11,43 @@ class getData{
         }
     };
     
+class ingredients{
+     constructor (malt, hops, yeast){
+        this.malt = malt;
+        this.hops = hops;
+        this.yeast = yeast;
+        }
+    };
+
     $('body').on('click', '.page-number', function(){
         $('.page-number').removeClass("active");
         $(this).addClass("active");
+        $('main').empty();
+        getBeer();
         })
      
     $('body').on('click', '.next', function(){
         if($('.active').next().hasClass('page-number')){
             $('.active').removeClass('active').next().addClass('active');}
+            $('main').empty();
+            getBeer();
         })
      
      
     $('body').on('click', '.previous', function(){
         if($('.active').prev().hasClass('page-number')){
             $('.active').removeClass('active').prev().addClass('active');}
+            $('main').empty();
+            getBeer();
         })
-    
-var url = "https://api.punkapi.com/v2/beers?page="+$('.active').text()+"&per_page=6";
+  
+function getBeer() {        
+    var url = "https://api.punkapi.com/v2/beers"
+    url += '?' + $.param({
+    'page': $('.active').text(),
+    'per_page': 6
+    });
+
 $.ajax({
     url: url,
     method: 'GET',
@@ -35,19 +55,30 @@ $.ajax({
         let beers = []
     data.forEach(element => {
         beers.push(new getData(element.image_url, element.name, element.description, element.abv, element.ibu, element.ph));
+        $('main').append('<div class="beer-item"><img src="'+element.image_url+'" ><h1>'+element.name+'</h1><p>'+element.description+'</p><section><ul><li>ABV</li><li><span>'+element.abv+'</span></li></ul><ul><li>IBU</li><li><span>'+element.ibu+'</span></li></ul><ul class="ph-color"><li>PH</li><li><span>'+element.ph+'</span></li></ul></section></div>');
     });
    console.log(beers);
     })
+}
 
- 
 $.ajax({
     url: "https://api.punkapi.com/v2/beers/random" ,
     method: 'GET',
     }).done(function(data) {
         let randomBeer = []
+        let flavour = []
     data.forEach(element => {
         randomBeer.push(new getData(element.image_url, element.name, element.description, element.abv, element.ibu, element.ph, element.tagline));
+        $('.randomBeer').append('<div class="one-beer"><div><img src="'+element.image_url+'" ></div><div class="description"><h1>'+element.name+'</h1><h2>'+element.tagline+'</h2><p>'+element.description+'</p><section><ul><li>ABV</li><li><span>'+element.abv+'</span></li></ul><ul><li>IBU</li><li><span>'+element.ibu+'</span></li></ul><ul class="ph-color"><li>PH</li><li><span>'+element.ph+'</span></li></ul></section><button>INGREDIENTS</button></div></div><button>GIVE ME ANOTHER BEER</button>');
+        flavour.push(new ingredients(element.ingredients.malt, element.ingredients.hops, element.ingredients.yeast));
     });
+
    console.log(randomBeer);
+   console.log(flavour);
     })
 
+
+$('.beerButton').on('click', function(){
+    $('main').css('display', 'none')
+
+});
